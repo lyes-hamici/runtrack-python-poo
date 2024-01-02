7419afffba196d786635100c5399439bda341862
lyes-hamici <lyes.hamici@laplateforme.io>
2024-01-02 10:25:11 +0100

    Job07

---
+++ Jour01/Job07/Personnage.py
@@ -0,0 +1,52 @@
+class Personnage:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+
+    def bas(self):
+        self.y = self.y - 1
+
+    def haut(self):
+        self.y = self.y + 1
+
+    def droite(self):
+        self.x = self.x + 1
+
+    def gauche(self):
+        self.x = self.x - 1
+
+    def position(self):
+        t = (self.x,self.y)
+        return f"Les coordonnées du personage sont {t}."
+    
+
+        
+
+p = Personnage(5,5)
+
+print("-----------------")
+
+print(p.position())
+
+print("-----------------")
+
+p.gauche()
+print("Aller a gauche",p.position())
+
+print("-----------------")
+
+p.droite()
+print("Aller a droite",p.position())
+
+print("-----------------")
+
+p.haut()
+print("Aller en haut",p.position())
+
+print("-----------------")
+
+p.bas()
+print("Aller en bas",p.position())
+
+print("-----------------")
