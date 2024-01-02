fc0c613540051d063a94c0ffd773d6c41cd01828
lyes-hamici <lyes.hamici@laplateforme.io>
2024-01-02 09:37:30 +0100

    Job02

---
+++ Jour01/Job02/Operation.py
@@ -0,0 +1,16 @@
+class Operation:
+    def __init__(self,nombre1,nombre2):
+        self.num1 = nombre1
+        self.num2 = nombre2
+
+
+op = Operation(12,3)
+
+
+
+print(op)
+print("----------------------")
+print("Le nombre1 est",op.num1)
+print("----------------------")
+print("Le nombre2 est",op.num2)
+print("----------------------")
