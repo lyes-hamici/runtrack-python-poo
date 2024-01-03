a57088c248b4971b58231cdc70b3d101d6380224
lyes-hamici <lyes.hamici@laplateforme.io>
2024-01-03 09:30:33 +0100

    Jour02 Job01

---
+++ Jour02/Job01/main.py
@@ -0,0 +1,48 @@
+class Rectangle:
+    def __init__(self,longueur,largeur) -> None:
+        self.__longueur = longueur
+        self.__largeur = largeur
+
+    def get_longueur(self):
+        return f"Longueur : {self.__longueur}"
+    
+
+    def set_longueur(self,n):
+        self.__longueur = n
+
+
+
+    def get_largeur(self):
+        return f"Largeur : {self.__largeur}"
+    
+
+    def set_largeur(self,n):
+        self.__largeur = n
+
+
+r = Rectangle(5,10)
+
+print("--------------------------")
+
+print(r.get_largeur())
+
+print("--------------------------")
+
+print(r.get_longueur())
+
+print("--------------------------")
+
+r.set_largeur(10)
+
+r.set_longueur(12)
+
+
+print(r.get_largeur())
+
+print("--------------------------")
+
+print(r.get_longueur())
+
+print("--------------------------")
+
+
