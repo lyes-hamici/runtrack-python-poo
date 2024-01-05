5453b48eadce7a800e0bd0016c96ea87a4fb5e48
lyes-hamici <lyes.hamici@laplateforme.io>
2024-01-05 11:45:50 +0100

    Jour04 Job05

---
+++ Jour04/Job05/Cercle.py
@@ -0,0 +1,28 @@
+import math
+from Forme import *
+
+class Cercle(Forme):
+    def __init__(self,rayon):
+        self.rayon = rayon
+
+
+    def changerRayon(self,n):
+        self.rayon = n
+
+    def circonférence(self):
+        circonférence_cercle = self.rayon * math.pi
+        return f"Circonférence du cercle {round(circonférence_cercle,2)}cm"
+
+    def aire(self):
+        aire_cercle = self.rayon**2 * math.pi
+        return f"Aire du cercle {round(aire_cercle,2)}cm²"
+    
+    def diametre(self):
+        diametre_cercle = self.rayon / math.pi
+        return f"Diametre du cercle {round(diametre_cercle,2)}cm"
+    
+    def afficherInfos(self):
+        return f"Le cercle possede un rayon de {self.rayon} cm, un diametre de {self.diametre()} cm, une aire de {self.aire()} cm² et une circonférence de {self.circonférence()} cm."
+    
+
+
---
+++ Jour04/Job05/main.py
@@ -0,0 +1,14 @@
+from Rectangle import *
+from Cercle import *
+
+
+rect = Rectangle(10,5)
+c = Cercle(7)
+
+print(rect.aire())
+
+print("-------------------------------------------------------------------------")
+
+print(c.aire())
+
+print("-------------------------------------------------------------------------")
